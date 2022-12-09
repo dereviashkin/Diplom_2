4c57ab9fb3b3a97556132e9de7fef83dd5ecd4a5
Валаев Георгий <ymnikgoga@gmail.com>
2022-12-10 00:54:21 +0300

    Разработаны ассерты на новые коды, ключи, значения

--- src/main/java/helpers/AssertHelper.java
+++ src/main/java/helpers/AssertHelper.java
@@ -5,8 +5,7 @@ import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.junit.Assert;
 
-import static date.TextResponses.getSomeFieldsAreRequired;
-import static date.TextResponses.getUserAlreadyExists;
+import static date.TextResponses.*;
 import static helpers.JsonHelper.checkResponseCode;
 import static helpers.JsonHelper.getValueByKey;
 
@@ -26,6 +25,13 @@ public class AssertHelper {
                 checkResponseCode(response));
     }
 
+    @Step("Проверка, что статус код - Не авторизован (401)")
+    public static void checkIfResponseCodeUnauthorized(Response response) {
+        Assert.assertEquals("Код ответа сервера не соответствует ожидаемому",
+                HttpStatus.SC_UNAUTHORIZED,
+                checkResponseCode(response));
+    }
+
     @Step("Проверка, что текст значения равен \"User already exists\"")
     public static void checkIfTextEqualsUserAlreadyExists(Response response) {
         Assert.assertEquals("Текст значения по ключу message не соответствет ожидаемому",
@@ -40,10 +46,24 @@ public class AssertHelper {
                 getValueByKey(response, "message"));
     }
 
+    @Step("Проверка, что текст значения равен \"email or password are incorrect\"")
+    public static void checkIfTextEqualsEmailOrPassAreIncorrect(Response response) {
+        Assert.assertEquals("Текст значения по ключу success не соответствет ожидаемому",
+                getEmailOrPassAreIncorrect(),
+                getValueByKey(response, "message"));
+    }
+
     @Step("Проверка, что текст значения равен \"true\"")
     public static void checkIfTextEqualsTrue(Response response) {
         Assert.assertEquals("Текст значения по ключу success не соответствет ожидаемому",
                 "true",
+                getValueByKey(response, "success"));
+    }
+
+    @Step("Проверка, что текст значения равен \"false\"")
+    public static void checkIfTextEqualsFalse(Response response) {
+        Assert.assertEquals("Текст значения по ключу success не соответствет ожидаемому",
+                "false",
                 getValueByKey(response, "success"));
     }
 }
