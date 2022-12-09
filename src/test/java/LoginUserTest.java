e20f37b508b316c217008b98f81c6615140f21f5
Валаев Георгий <ymnikgoga@gmail.com>
2022-12-10 00:58:37 +0300

    Добавлены позитивные и негативные кейсы проверки логина

--- src/test/java/LoginUserTest.java
+++ src/test/java/LoginUserTest.java
@@ -1,11 +1,38 @@
 import io.qameta.allure.Description;
+import io.restassured.response.Response;
 import org.junit.Test;
+
+import static helpers.AssertHelper.*;
+import static helpers.UserHelper.*;
 
 public class LoginUserTest extends BaseTest {
 
     @Test
-    @Description("Позитивный тест логина существующей учетной записью")
+    @Description("Позитивный тест авторизации существующей учетной записью")
     public void loginUserExistsSuccess() {
+        createNewUserAndRegisterHim();
+        Response response = getRegisteredUserAndLogin();
+        checkResponseCodeOk(response);
+        checkIfTextEqualsTrue(response);
+    }
 
+    @Test
+    @Description("Негативный тест авторизации с неверным паролем")
+    public void loginUserExistsIncorrectPassFail() {
+        createNewUserAndRegisterHim();
+        Response response = getRegisteredUserIncorrectPasswordAndLogin();
+        checkIfResponseCodeUnauthorized(response);
+        checkIfTextEqualsFalse(response);
+        checkIfTextEqualsEmailOrPassAreIncorrect(response);
+    }
+
+    @Test
+    @Description("Негативный тест авторизации с неверным email")
+    public void loginUserExistsIncorrectEmailFail() {
+        createNewUserAndRegisterHim();
+        Response response = getRegisteredUserIncorrectEmailAndLogin();
+        checkIfResponseCodeUnauthorized(response);
+        checkIfTextEqualsFalse(response);
+        checkIfTextEqualsEmailOrPassAreIncorrect(response);
     }
 }
