d70ca1f62645e65327e71ab3f765739d6caecc74
Валаев Георгий <ymnikgoga@gmail.com>
2022-12-09 23:35:08 +0300

    Создан класс LoginUserTest.java

---
+++ src/test/java/LoginUserTest.java
@@ -0,0 +1,11 @@
+import io.qameta.allure.Description;
+import org.junit.Test;
+
+public class LoginUserTest extends BaseTest {
+
+    @Test
+    @Description("Позитивный тест логина существующей учетной записью")
+    public void loginUserExistsSuccess() {
+
+    }
+}
