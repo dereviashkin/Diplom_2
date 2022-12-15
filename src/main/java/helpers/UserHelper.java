070e2597d0e490e1ba9bb7133bdd0e792d01fcc2
Валаев Георгий <ymnikgoga@gmail.com>
2022-12-16 00:34:20 +0300

    Класс UserHelper.java переработаны методы с switch, убраны методы в RestHelper, ручки через константу, а не геттер

--- src/main/java/helpers/UserHelper.java
+++ src/main/java/helpers/UserHelper.java
@@ -4,15 +4,13 @@ import entities.User;
 import io.qameta.allure.Step;
 import io.restassured.response.Response;
 
-import static date.Endpoints.getLogin;
-import static date.Endpoints.getRegister;
+import static date.Endpoints.login;
+import static date.Endpoints.register;
+import static helpers.AuthHelper.*;
 import static helpers.GeneratorHelper.*;
-import static helpers.JsonHelper.sendPostRequest;
+import static helpers.RestHelper.sendPostRequest;
 
 public class UserHelper {
-
-    private static String password;
-    private static String email;
 
     @Step("Гененируем случаного пользователя")
     private static User generateRandomUser() {
@@ -21,18 +19,21 @@ public class UserHelper {
 
     @Step("Гененируем случаного пользователя без одного из полей")
     private static User generateRandomUserWithEmptyField(String field) {
+        User user;
         switch (field) {
             case "name":
-                return new User("", randomEmail(), randomPassword());
+                user = new User("", randomEmail(), randomPassword());
+                break;
             case "email":
-                return new User(randomName(), "", randomPassword());
+                user = new User(randomName(), "", randomPassword());
+                break;
             case "password":
-                return new User(randomName(), randomEmail(), "");
-//            default:
-//                return new User(randomName(), randomEmail(), randomPassword());
+                user = new User(randomName(), randomEmail(), "");
+                break;
+            default:
+                throw new RuntimeException("Задано неизвестное поле для User");
         }
-        //TODO костыль, не знаю пока как поправить
-        return null;
+        return user;
     }
 
     @Step("Используем существующего пользователя")
@@ -43,44 +44,63 @@ public class UserHelper {
     @Step("Берем заведомо существующие в базе данные и отправляем их на регистрацию")
     public static Response createExistingUserAndRegisterHim() {
         User user = existingUser();
-        return sendPostRequest(user, getRegister());
+        return sendPostRequest(user, register);
     }
 
     @Step("Берем сгенерированные данные пользователя и отправляем их на регистрацию")
     public static Response createNewUserAndRegisterHim() {
         User user = generateRandomUser();
         saveEmailAndPassword(user);
-        return sendPostRequest(user, getRegister());
+        Response response = sendPostRequest(user, register);
+        saveAccessToken(response);
+        return response;
     }
 
     @Step("Берем сгенерированные данные пользователя с пустым полем и отправляем их на регистрацию")
     public static Response createNewUserWithEmptyFieldAndRegisterHim(String field) {
         User user = generateRandomUserWithEmptyField(field);
         saveEmailAndPassword(user);
-        return sendPostRequest(user, getRegister());
+        return sendPostRequest(user, register);
     }
 
-    @Step("Сохраняем почту и пароль для последующего логина")
-    public static void saveEmailAndPassword(User user) {
-        email = user.getEmail();
-        password = user.getPassword();
-    }
-
+    //TODO переделать: нужен текущий экземпляр класса User, а не новый
     @Step("Берем созданного пользователя и логинимся им")
     public static Response getRegisteredUserAndLogin() {
-        User user = new User(email, password);
-        return sendPostRequest(user, getLogin());
+        User user = new User(getEmail(), getPassword());
+        return sendPostRequest(user, login);
     }
 
+    //TODO переделать: нужен текущий экземпляр класса User с обнуленным полем, а не новый
     @Step("Берем созданного пользователя, ломаем пароль и логинимся им")
     public static Response getRegisteredUserIncorrectPasswordAndLogin() {
-        User user = new User(email, "");
-        return sendPostRequest(user, getLogin());
+        User user = new User(getEmail(), "");
+        return sendPostRequest(user, login);
     }
 
+    //TODO переделать: нужен текущий экземпляр класса User с обнуленным полем, а не новый
     @Step("Берем созданного пользователя, ломаем email и логинимся им")
     public static Response getRegisteredUserIncorrectEmailAndLogin() {
-        User user = new User("", password);
-        return sendPostRequest(user, getLogin());
+        User user = new User("", getPassword());
+        return sendPostRequest(user, login);
+    }
+
+    //TODO переделать: нужен текущий экземпляр класса User с рандомным полем, а не новый
+    @Step("Берем созданного пользователя и меняем ему имя")
+    public static User changeRegisteredUserFieldRandom(String fieldToRandom) {
+        User user;
+        switch (fieldToRandom) {
+            case "name":
+                user = new User(randomName(), null, null);
+                break;
+            case "email":
+                user = new User(null, randomEmail(), null);
+                break;
+            case "password":
+                user = new User(null, null, randomPassword());
+                break;
+            default:
+                throw new RuntimeException("Задано неизвестное поле для User");
+        }
+        return user;
     }
 }
